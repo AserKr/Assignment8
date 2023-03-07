c9d31e955a93645631d7a2893e7d402cd6867217
Aser Kroma <asser.krouma232@gmail.com>
2023-03-07 10:31:44 +0000

    Created author Class

---
+++ src/main/java/hi/HBV202G/Author.java
@@ -0,0 +1,18 @@
+package hi.HBV202G;
+
+public class Author {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String name;
+
+
+    public Author(String name) {
+        this.name = name;
+    }
+}
