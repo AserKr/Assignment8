f98452ba94a7b7a357cf2b1f0e08ed109925410c
Vilborg Erlendsdóttir <vie15@hi.is>
2023-03-07 10:59:37 +0000

    Wrote the classes User, Student and FacultyMember

---
+++ src/main/java/hi/HBV202G/Lending.java
@@ -0,0 +1,11 @@
+package hi.HBV202G;
+
+import java.time.LocalDate;
+
+public class Lending {
+    private LocalDate dueDate;
+
+    public Lending(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+}
